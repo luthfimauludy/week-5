07e0a57c14f18cd9db8f91e8da3e2f99ea2bc44c
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2024-02-06 09:45:35 +0700

    add: installing cors

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 
 // Import routes
@@ -11,6 +12,7 @@ const User = require("./server/api/user");
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -20,6 +22,13 @@ app.use("/api", City);
 app.use("/api", Category);
 app.use("/api", Auth);
 app.use("/api", User);
+
+app.get("/", (req, res) => {
+  return res.json({
+    success: true,
+    message: "Backend is running well",
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
